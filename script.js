7c1078b9dea00d881ebd3d63d9971fa8f740c7e5
allycesouza <vitoriaallyce04@gmail.com>
2025-06-13 02:33:17 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarMensagem() {
+ const mensagem = document.getElementById("mensagem");
+ mensagem.textContent = "Seja bem-vindo(a)! É um prazer receber sua visita em minha página. 🌟";
+ mensagem.classList.add("mostrar");
+
+}
